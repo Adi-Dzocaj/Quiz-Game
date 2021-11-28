e5715f4a8d489bd1c5669c35e2a91485fdbfd215
Adi Dzocaj <adidzocaj@gmail.com>
2021-11-28 20:19:59 +0100

    Added a fastest time component to the game

--- script.js
+++ script.js
@@ -20,144 +20,9 @@ const students = [
 		"name" : "Benjamin Tsubarah",
 		"image": "assets/images/students/benjamin-tsubarah.jpg",
 	},
-	{
-		"name" : "Calle Nilsson",
-		"image": "assets/images/students/calle-nilsson.jpg",
-	},
-	{
-		"name" : "Chikage Takahashi Molander",
-		"image": "assets/images/students/chikage-takahashi-molander.jpg",
-	},
-	{
-		"name" : "Daniel Be",
-		"image": "assets/images/students/daniel-be.jpg",
-	},
-	{
-		"name" : "Daniel Carlsson",
-		"image": "assets/images/students/daniel-carlsson.jpg",
-	},
-	{
-		"name" : "Elin Ahlgren",
-		"image": "assets/images/students/elin-ahlgren.jpg",
-	},
-	{
-		"name" : "Emma Käck",
-		"image": "assets/images/students/emma-kack.jpg",
-	},
-	{
-		"name" : "Eric Ståhl",
-		"image": "assets/images/students/eric-stahl.jpg",
-	},
-	{
-		"name" : "Frans Gustavson Påsse",
-		"image": "assets/images/students/frans-gustavson-passe.jpg",
-	},
-	{
-		"name" : "Glafira Veretennikova",
-		"image": "assets/images/students/glafira-veretennikova.jpg",
-	},
-	{
-		"name" : "Gustaf Grönlund",
-		"image": "assets/images/students/gustaf-gronlund.jpg",
-	},
-	{
-		"name" : "Hanna Håkanson",
-		"image": "assets/images/students/hanna-hakanson.jpg",
-	},
-	{
-		"name" : "Heidi Sjöberg",
-		"image": "assets/images/students/heidi-sjoberg.jpg",
-	},
-	{
-		"name" : "Hugo Carzborn",
-		"image": "assets/images/students/hugo-carzborn.jpg",
-	},
-	{
-		"name" : "Jesper Kling",
-		"image": "assets/images/students/jesper-kling.jpg",
-	},
-	{
-		"name" : "Johan Ranestam",
-		"image": "assets/images/students/johan-ranestam.jpg",
-	},
-	{
-		"name" : "Johanna Bäckström",
-		"image": "assets/images/students/johanna-backstrom.jpg",
-	},
-	{
-		"name" : "Johanna Jönsson",
-		"image": "assets/images/students/johanna-jonsson.jpg",
-	},
-	{
-		"name" : "Jona Torsson",
-		"image": "assets/images/students/jona-torsson.jpg",
-	},
-	{
-		"name" : "Josefine Ahlstedt",
-		"image": "assets/images/students/josefine-ahlstedt.jpg",
-	},
-	{
-		"name" : "Julia Jespersdotter Högman",
-		"image": "assets/images/students/julia-jespersdotter-hogman.jpg",
-	},
-	{
-		"name" : "Julia Nemell",
-		"image": "assets/images/students/julia-nemell.jpg",
-	},
-	{
-		"name" : "Linus Lindberg",
-		"image": "assets/images/students/linus-lindberg.jpg",
-	},
-	{
-		"name" : "Malin Olsson",
-		"image": "assets/images/students/malin-olsson.jpg",
-	},
-	{
-		"name" : "Maria Haara-Lundhammar",
-		"image": "assets/images/students/maria-haara-lundhammar.jpg",
-	},
-	{
-		"name" : "Maria Lövgren",
-		"image": "assets/images/students/maria-lovgren.jpg",
-	},
-	{
-		"name" : "Nikola Dimitrijoski",
-		"image": "assets/images/students/nikola-dimitrijoski.jpg",
-	},
-	{
-		"name" : "Paulina Kiendys",
-		"image": "assets/images/students/paulina-kiendys.jpg",
-	},
-	{
-		"name" : "Raymond Lam",
-		"image": "assets/images/students/raymond-lam.jpg",
-	},
-	{
-		"name" : "Robin Karlsson",
-		"image": "assets/images/students/robin-karlsson.jpg",
-	},
-	{
-		"name" : "Sara Almqvist",
-		"image": "assets/images/students/sara-almqvist.jpg",
-	},
-	{
-		"name" : "Tim Nilsson",
-		"image": "assets/images/students/tim-nilsson.jpg",
-	},
-	{
-		"name" : "Tirapat Sukjit",
-		"image": "assets/images/students/tirapat-sukjit.jpg",
-	},
-	{
-		"name" : "Tobias Silfverberg",
-		"image": "assets/images/students/tobias-silfverberg.jpg",
-	},
-	{
-		"name" : "Wiktoria Dobrzewinska",
-		"image": "assets/images/students/wiktoria-dobrzewinska.jpg",
-	},
 ];
 
+// An array containing the students that didnt publish pictures. I left it inside the document even though it was never used.
 const missing_students = [
 	{
 		"name": "Andjela Saponjic",
@@ -181,10 +46,21 @@ const missing_students = [
 	},
 ];
 
-// Setting the amount of guesses, right answers and the highscore to zero.
-let amountOfGuesses = 0;
+// Setting the amount of guesses, right answers, time passed and the highscore to zero.
+let amountOfGuessesMade = 0;
+let amountOfTotalGuesses = students.length;
 let rightAnswers = 0;
 let highScore = 0;
+let bestTime = 0;
+let timePassed = 1;
+
+// Calculating how many minutes and seconds it has took for the user to get through the round.
+let minutes = Math.floor(timePassed / 60);
+let seconds = timePassed % 60;
+
+// Declaring variables for storage of the best result.
+let bestMinutes = Math.floor(bestTime / 60);
+let bestSeconds = bestTime % 60;
 
 // Setting an empty array that contains all given answers.
 let givenAnswersArr = [];
@@ -203,16 +79,30 @@ const correctionContainer = document.querySelector('.correctionContainer');
 const winningMessage = document.querySelector('.winningMessage');
 
 // Creating paragraphs and appending them to the results display.
-const scoreParagraph = document.createElement("P");
+const scoreParagraph = document.createElement('P');
 resultDisplay.appendChild(scoreParagraph);
 const highScoreParagraph = document.createElement('P');
 resultDisplay.appendChild(highScoreParagraph);
+const timeParagraph = document.createElement('P');
+resultDisplay.appendChild(timeParagraph);
+const bestTimeParagraph = document.createElement('P');
+resultDisplay.appendChild(bestTimeParagraph);
+
+// Function that adds 1 to the seconds variable.
+function incrementSeconds() {
+	timePassed += 1;
+}
+
+// Using the setInterval method and passing in the incrementSeconds function aswell as 1000ms in interval, so that a 1 is added to the seconds variable, every 1000ms.
+let interval = setInterval(incrementSeconds, 1000);
 
 // Giving the highscore container a bootstrap class of d-none so that it wont be present at the start.
 highScoreContainer.classList.add('d-none');
 // Giving the highscore paragraph & score paragraph text-align-center through bootstrap class text-center.
 highScoreParagraph.classList.add('text-center');
 scoreParagraph.classList.add('text-center');
+timeParagraph.classList.add('text-center');
+bestTimeParagraph.classList.add('text-center');
 
 // Creating element 'button' and appending it to the startbutton container.
 const startButton = document.createElement("BUTTON");
@@ -291,7 +181,7 @@ guessingSection.addEventListener('click', e => {
 			// Increasing the amount of right answers by 1.
 			rightAnswers++;
 			// Increasing the amount of total guesses by 1.
-			amountOfGuesses++;
+			amountOfGuessesMade++;
 		} 
 		// Adding a conditional which checks if the user gets the answer wrong.
 		else {
@@ -299,7 +189,7 @@ guessingSection.addEventListener('click', e => {
 			// Initializing new round.
 			startRound();
 			// Increasing the amount of total guesses by 1.
-			amountOfGuesses++;
+			amountOfGuessesMade++;
 		}
 	}
 
@@ -310,7 +200,9 @@ guessingSection.addEventListener('click', e => {
 	})
 
 	// Checking for when the game is supposed to finish
-	if (amountOfGuesses === students.length) {
+	if (amountOfGuessesMade === students.length) {
+	// Stopping the setInterval from counting.
+	clearInterval(interval);
 	// Removing the margin from in between the image and the buttons, when the game is over, in order to match the stats menu with the image. 
 	guessingSection.classList.remove('mt-3');
 	// Printing out the filtered out wrong answers to the DOM and styling the containers.
@@ -341,21 +233,46 @@ guessingSection.addEventListener('click', e => {
 		highScore = rightAnswers;
 		// Replacing the old highscore paragraph with the new one.
 		highScoreParagraph.innerHTML = "";
-		highScoreParagraph.innerHTML += `Nice! New highscore: ${highScore} guesses!`;
+		highScoreParagraph.innerHTML += `Nice! New highscore: ${highScore}/${amountOfGuessesMade} correct answers!`;
 	} 
 	// Checking for every time that a new highscore isn't reached.
-	else {
+	else if (rightAnswers < highScore){
 		// Telling the user that their highscore is still the same as their previous one.
 		highScoreParagraph.innerHTML = "";
-		highScoreParagraph.innerHTML += `Highscore is still: ${highScore} guesses.`;
+		highScoreParagraph.innerHTML += `Highscore is still: ${highScore}/${amountOfGuessesMade} correct answers.`;
 	}
 	// if the user gets all of the guesses right, display a winning message instead of a correction instruction.
-	if (rightAnswers === amountOfGuesses){
+	if (rightAnswers === amountOfGuessesMade){
 		winningMessage.classList.remove('d-none');
 		correctionInstructions.classList.add('d-none');
+
+		minutes = Math.floor(timePassed / 60);
+		seconds = timePassed % 60;
+		console.log(timePassed)
+
+		console.log(timePassed)
+		timeParagraph.innerHTML = `Time: ${minutes}m ${seconds}s`;
+		if (timePassed > 0 & bestTime === 0) {
+			bestMinutes = minutes;
+			bestSeconds = seconds;
+			bestTime = timePassed;
+			bestTimeParagraph.innerHTML = ``;
+			bestTimeParagraph.innerHTML = `New highscore: ${highScore}/${amountOfTotalGuesses} correct answers in: ${bestMinutes}m ${bestSeconds}s!`	
+
+		}
+		if (bestTime > timePassed) {
+			bestMinutes = minutes;
+			bestSeconds = seconds;		
+			bestTime = timePassed;
+			bestTimeParagraph.innerHTML = ``;
+			bestTimeParagraph.innerHTML = `New highscore: ${highScore}/${amountOfTotalGuesses} correct answers in: ${bestMinutes}m ${bestSeconds}s!`	
+		}
+	}
+	if (rightAnswers === amountOfGuessesMade & highScore === amountOfGuessesMade){
+		highScoreParagraph.innerHTML = '';
 	}
 	// Telling the user how many points he/she scored.
-	scoreParagraph.innerHTML = (`Result: ${rightAnswers}/${amountOfGuesses} correct answers!`);
+	scoreParagraph.innerHTML = (`Result: ${rightAnswers}/${amountOfGuessesMade} correct answers!`);
 }
 })
 
@@ -363,6 +280,28 @@ guessingSection.addEventListener('click', e => {
 resultDisplay.addEventListener('click', e => {
 	// Checking if the click-target is on the button.
 	if (e.target.tagName === ('BUTTON')) {
+	// Creating more generic best time message, while the new game is running.
+	// bestTimeParagraph.innerHTML = `Best time: ${bestMinutes}m ${bestSeconds}s`;
+	// Resetting the time passed so that a new count can begin.
+	seconds = 0;
+	minutes = 0;
+	timePassed = 1;
+
+	// Adding a clearInterval prior to starting the count again. This fixed a bug which entailed there being a possibility for interval counters to stack, if user mistakenly clicked the new game button twice. Adding a clearInterval before initializing a new count fixed this problem.
+	clearInterval(interval);
+	// Function that adds a 1 to the seconds variable and prints a paragraph out into the results section.
+	function incrementSeconds() {
+		console.log(timePassed)
+		timePassed += 1;
+	}
+
+	// Using the setInterval method and passing in the incrementSeconds function aswell as 1000ms in interval, so that a 1 is added to the seconds variable, every 1000ms.
+	interval = setInterval(incrementSeconds, 1000);
+
+	// Clearing the timeParagraph & bestTimeParagraph on reset.
+	timeParagraph.innerHTML = '';
+	bestTimeParagraph.innerHTML = '';
+
 	// Returning the display:none to the winningMessage and therefore resetting it.
 	winningMessage.classList.add('d-none');
 	// Adding the margin back from in between buttons and image.
@@ -376,12 +315,16 @@ resultDisplay.addEventListener('click', e => {
 	givenAnswersArr = [];
 	// Shuffling students array so that the order is once again randomised.
 	shuffleArrayFunction(students);
-	// Replacing the highscore paragraph with a more generic one to look at during the new game.
-	highScoreParagraph.innerHTML = `Highscore: ${highScore} correct answers!`;
+	if (highScore === amountOfGuessesMade) {
+		highScoreParagraph.innerHTML = `Highscore: ${highScore}/${amountOfTotalGuesses} correct answers in: ${bestMinutes}m ${bestSeconds}s`
+	} else {
+		// Replacing the highscore paragraph with a more generic one to look at during the new game.
+		highScoreParagraph.innerHTML = `Highscore: ${highScore}/${amountOfTotalGuesses} correct answers`;
+	}
 	// Removing the paragraph that details the score, since we are in the process of generating a new one.
 	scoreParagraph.innerHTML = "";
 	// Setting the amount of guesses to 0.
-	amountOfGuesses = 0;
+	amountOfGuessesMade = 0;
 	// Setting the amount of right answers to 0.
 	rightAnswers = 0;
 	// Initializing the start round function and all of its included code.
